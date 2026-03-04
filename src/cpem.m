f3920dda09fa40fa9ea272836da8642cf8252c6f
Jeremy W. Hopwood <jeremy@hopwoodclan.org>
2026-03-04 10:35:51 -0600

    misc updates and organization

--- src/cpem.m
+++ src/cpem.m
@@ -1,4 +1,20 @@
 function M = cpem(a)
 %cpem
-    M = [0,-a(3),a(2);a(3),0,-a(1);-a(2),a(1),0];
+%
+% Copyright (c) 2026 Jeremy W. Hopwood. All rights reserved.
+%
+% This function computes the cross product eqwuivalent matrix of a 3x1 
+% vector a. For two vectors a and b, it satisfies cpem(a)*b = cross(a,b).
+%
+% Inputs:
+%
+%   a       A 3x1 vector
+%  
+% Outputs:
+%
+%   M       The cross product equivalent matrix of the vector a
+%
+
+M = [0,-a(3,1),a(2,1); a(3,1),0,-a(1,1); -a(2,1),a(1,1),0];
+
 end
--- src/cpeminv.m
+++ src/cpeminv.m
@@ -1,5 +1,30 @@
 function a = cpeminv(M)
 %cpeminv
-Ma = 0.5*(M - M');
-a = [Ma(3,2); Ma(1,3); Ma(2,1)];
+%
+% Copyright (c) 2026 Jeremy W. Hopwood. All rights reserved.
+%
+% This function computes the inverse operation to the cross product
+% equivalent matrix, often called the "vee map" (a reference to the Lie
+% algebra so(3) and its one-to-one correspondence with 3-dimensional
+% Eucidean space). Valid for any 3x3 matrix M, cpeminv(M) takes the
+% skew-symmetric part of M and, treating it as a cross product equivalent
+% matrix, returns the 3x1 vector that corresponds to it.
+%
+% Inputs:
+%
+%   M       A 3x3 matrix
+%  
+% Outputs:
+%
+%   a       The vee map of M
+%
+
+% Skew symmetric part of the matrix M. Note that any square matrix can be
+% decomposed into its symmetric part 0.5*(M+M') and its skew-symmetric part
+% 0.5*(M-M') such that M = 0.5*(M+M') + 0.5*(M-M').
+Mskew = 0.5*(M - M');
+
+% 3x1 vector correspoinding to the cross product equivalent matrix Mskew
+a = [Mskew(3,2); Mskew(1,3); Mskew(2,1)];
+
 end
